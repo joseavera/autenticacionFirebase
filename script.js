0174b02dfbc2450a93592a05823d33c6d1813245
@joseavera <69088516+joseavera@users.noreply.github.com>
2025-06-09 17:41:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 // PASO 1: CONFIGURACIÓN DE FIREBASE (¡REEMPLAZA CON TUS PROPIAS CREDENCIALES!)
   const firebaseConfig = {
-    apiKey: "AIzaSyBZEVISZuRfn1iC3be5b7xTMQiCZubXyTU",
-    authDomain: "bdnosql-de619.firebaseapp.com",
-    projectId: "bdnosql-de619",
-    storageBucket: "bdnosql-de619.firebasestorage.app",
-    messagingSenderId: "519369652548",
-    appId: "1:519369652548:web:260c2b45275f5a0ff5ff11",
-    measurementId: "G-1FYEJLG220"
+
   };
 
 // Inicializa Firebase
